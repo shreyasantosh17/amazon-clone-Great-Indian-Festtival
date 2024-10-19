933ff125c35f946b5a51d7adf1566572c61a7585
avanimathur <avanimathur24@gmail.com>
2024-10-19 07:38:56 +0530

    upload project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# amazon-clone-Great-Indian-Festtival
+# Amazon-Clone
+<hr>
+https://avanimathur.github.io/Amazon-Clone/
+<br>
+This project is a replica of the front-end of the popular e-commerce platform Amazon. 
+<br>
+<b>Technologies Used : </b> HTML | CSS | JavaScript 
---
+++ app.js
@@ -0,0 +1,23 @@
+const right=document.querySelector(".right-btn");
+right.addEventListener("click",function(event){
+const conent=document.querySelector(".slide-carousel");
+conent.scrollLeft+=1100;
+event.preventDefault();
+})
+
+const left=document.querySelector(".left-btn");
+left.addEventListener("click",function(event){
+const conent=document.querySelector(".slide-carousel");
+conent.scrollLeft-=1100;
+event.preventDefault();
+})
+
+const toTopButton = document.querySelector('.backtotop');
+
+toTopButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
